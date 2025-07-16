f17188ba53fbcca3a81a88f145fab05aeb63b175
Alejandro Guerra <alexguerran2002@gmail.com>
2025-07-16 11:36:14 +0200

    Primer Hola Mundo mas dinujo en ASCII

---
+++ HolaMundoSolucion1/HolaMundo1/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        // Printar por consola.
+        Console.WriteLine("Saludos desde Summer Camp ");
+
+
+        
+Console.WriteLine(@"             _   |~  _");
+Console.WriteLine(@"            [_]--'--[_]");
+Console.WriteLine(@"            |'|""`""|'|");
+Console.WriteLine(@"            | | /^\ | |");
+Console.WriteLine(@"            |_|_|I|_|_|");
+
+    }
+}
