d4fd3daa1b6e6e2a5d6f652838457534ba45f2d3
Alejandro Guerra <alexguerran2002@gmail.com>
2025-07-17 09:41:14 +0200

    Añadido ejercicio 1 sobre bucles y switch

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,45 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Introduce un nunmero del 1 al 7 (o 0 para salir):");
+
+        while (true)
+        {
+            int numero = int.Parse(Console.ReadLine());
+
+            // Bucle para seguir pidiendo números hasta que se introduzca 0
+
+            switch (numero)
+            {
+                case 1:
+                    Console.WriteLine("Lunes");
+                    break;
+                case 2:
+                    Console.WriteLine("Martes");
+                    break;
+                case 3:
+                    Console.WriteLine("Miércoles");
+                    break;
+                case 4:
+                    Console.WriteLine("Jueves");
+                    break;
+                case 5:
+                    Console.WriteLine("Viernes");
+                    break;
+                case 6:
+                    Console.WriteLine("Sábado");
+                    break;
+                case 7:
+                    Console.WriteLine("Domingo");
+                    break;
+                case 0:
+                    Console.WriteLine("Hasta luego!!");
+                    return;
+                default:
+                    Console.WriteLine("Valor no valido, intenta de nuevo.");
+                    break;
+            }
+        }
+    }
+}
